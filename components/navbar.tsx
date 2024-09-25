9fe2b9b92a8a0128c0650ba88dcbedf02e68c194
Ngwane-Sanele <masumpangwame@gmail.com>
2024-09-25 02:26:29 +0000

    Added in

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -216,17 +216,17 @@ export const Navbar = () => {
           <UserButton />
           
         </SignedIn>
-        <SignedOut>
-          <SignInButton>
-            <button
-              className="text-lg font-semibold rounded-lg px-4 py-2 transition-all duration-300 ease-in-out bg-gradient-to-r from-green-500 to-green-500 text-gray-100 shadow-lg hover:from-green-600 hover:to-green-600 dark:bg-gradient-to-r dark:from-green-800 dark:to-green-800 dark:text-gray-100 dark:hover:from-green-900 dark:hover:to-green-900 focus:outline-none"
-              type="button"
-            >
-              Sign In
-            </button>
-          </SignInButton>
-        </SignedOut>
-        <ThemeSwitch />
+          <SignedOut>
+            <SignInButton>
+              <button
+                className="text-lg font-semibold rounded-lg px-4 py-2 transition-all duration-300 ease-in-out bg-gradient-to-r from-green-500 to-green-500 text-gray-100 shadow-lg hover:from-green-600 hover:to-green-600 dark:bg-gradient-to-r dark:from-green-800 dark:to-green-800 dark:text-gray-100 dark:hover:from-green-900 dark:hover:to-green-900 focus:outline-none"
+                type="button"
+              >
+                SignIn
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <ThemeSwitch />
         <NavbarMenuToggle onClick={() => setMenuOpen(!menuOpen)} />
       </NavbarContent>
 

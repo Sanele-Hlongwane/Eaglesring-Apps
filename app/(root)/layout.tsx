d4384f52a550de488bd93d93aded226ab945d1d5
Sanele Hlongwane <135650019+Sanele-Hlongwane@users.noreply.github.com>
2024-10-03 18:37:04 +0000

    Fixed Fixed meetingpage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from "react";
 
-import StreamVideoProvider from "@/providers/StreamClientProvider";
-
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return <main className="text-xs md:text-base font-roboto">{children}</main>;
 };
